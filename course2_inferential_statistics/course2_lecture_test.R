ce2f84d37b96b1537a4f1642b663f38d1602d913
nealxun <nealxun@gmail.com>
2019-08-27 22:01:22 -0700

    complete course 2 week 3 lab

--- course2_inferential_statistics/course2_lecture_test.R
+++ course2_inferential_statistics/course2_lecture_test.R
@@ -20,5 +20,38 @@ library(statsr)
 
 
 # week1 -------------------------------------------------------------------
-# find critical value based on percentie
-qnorm(0.99)
+# z distribution, critical value
+qnorm(0.975)
+
+# z disbritution, p value, two tail
+2 * pnorm(1.96, lower.tail = FALSE)
+
+# t distribution, critical value
+qt(0.025, df = 21)
+
+# t distribution, p value, two tail
+2 * pt(2.24, df = 21, lower.tail = FALSE)
+
+
+
+
+# week3 -------------------------------------------------------------------
+# practice quiz
+# q5
+pt(0.5, df = 18-1, lower.tail = FALSE)
+
+# quiz
+# q8
+2 * pt(2.485, df = 26-1, lower.tail = FALSE)
+
+# q11
+75/40.13
+
+
+
+
+
+
+
+
+
